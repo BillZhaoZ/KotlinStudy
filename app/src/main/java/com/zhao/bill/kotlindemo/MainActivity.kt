3c6ebe6aed2e2d4ed97d30393eec4bc87f75db62
BillZhaoZ <zzbillcumt@126.com>
2017-10-17 18:29:44 +0800

    完善代码：添加吐司

--- app/src/main/java/com/zhao/bill/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/zhao/bill/kotlindemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
 
         tv_hello_world.setOnClickListener(this)
+        tv_toast.setOnClickListener(this)
 
     }
 
@@ -28,7 +30,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         // 跳转Helloworld
             R.id.tv_hello_world -> startActivity(Intent(this, HelloWorldActivity::class.java))
 
-
+        // 吐司
+            R.id.tv_toast -> Toast.makeText(this, "Okay,you got me here!!!", Toast.LENGTH_SHORT).show()
         }
     }
 
